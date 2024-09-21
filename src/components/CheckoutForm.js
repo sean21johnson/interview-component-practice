bfad214f9b5de0602e4c124cb3db12e76eaea3ec
Sean Johnson <sean.johnson@zenhub.com>
2024-09-21 14:01:30 -0400

    Validation updates and localStorage persistence

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -6,30 +6,25 @@
   4. Allow the user to save the form data in local storage for future use
 */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const isOddNumber = (number) => {
-  if (number === 0) {
-    return false;
-  } else if (number % 2 > 0) {
-    return true;
-  }
-
-  return false;
-}
+const isOddIndex = (index) => index % 2 === 1;
 
 const subtractNine = (number) => number - 9;
 
-const doubleNumber = (num) => num * 2;
+const doubleNumber = (number) => number * 2;
+
+const checkModuloTen = (number) => number % 10 === 0;
 
 const updateNumber = (number) => {
-  const doubleDigit = doubleNumber(number);
+  const doubleValue = doubleNumber(number);
 
-  if (doubleDigit > 9) {
-    return subtractNine(doubleDigit);
+  if (doubleValue > 9) {
+    console.log('triggered')
+    return subtractNine(doubleValue);
   }
 
-  return doubleDigit;
+  return doubleValue;
 }
 
 const sumNumbers = (numbers) => {
@@ -38,29 +33,17 @@ const sumNumbers = (numbers) => {
   }, 0)
 }
 
-  // Validate using the Luhn algorithm
-  const validateCreditCardNumber = (creditCardNumber) => {
-    // Remember that the creditCardNumber is a string originally
-    // Reverse the digits of the credit card number
-    const reversedNumbers = creditCardNumber.split('').reverse().map(number => Number(number));
+const sanitizeCreditCardInput = (creditCardNumber) => {
+  /*
+   - DELIMITER: / is a delimiter. It indicates the start and end of the regular expression pattern. In this case, '/\D/'
+   - PATTERN: \D is a pattern in regex that matches any character that is NOT a digit
+   - ESCAPE CHARACTER: The "\" here is an escape character. It tells the JS engine to treat the character after it in a special way. 
+   - FLAGS (OPTIONAL): g is the global flag. This ensures that the regex is applied to the entire string, not just the first match. Goes after the pattern
+  */
 
-    const updatedNumbers = [];
-
-    reversedNumbers.forEach((number, index) => {
-      const isSecondIndex = isOddNumber(index);
-      let updatedNumber = number;
-
-      if (isSecondIndex) {
-        updatedNumber = updateNumber(number)
-      }
-
-      updatedNumbers.push(updatedNumber);
-    })
-
-    const sum = sumNumbers(updatedNumbers);
-
-    console.log({sum})
-  }
+  // We use replace to find all the matches to non-numbers and then replace them with an empty string which is the equivalent of nothing
+  return creditCardNumber.replace(/\D/g, '');
+}
 
 function CheckoutForm() {
   // Set state
@@ -71,15 +54,60 @@ function CheckoutForm() {
   })
   const [errorMessage, setErrorMessage] = useState('');
 
+  useEffect(() => {
+    const savedData = localStorage.getItem('formData');
+    // Remember that localStorage only stores string values
+    // We need to parse the string and turn it back into an object
+    const stringified = JSON.parse(savedData);
+
+    console.log({stringified})
+
+    console.log({savedData})
+  })
+
+    // Validate using the Luhn algorithm
+    const validateCreditCardNumber = () => {
+      const { creditCard } = formData;
+
+      const sanitizedCard = sanitizeCreditCardInput(creditCard);
+
+      if (sanitizedCard.length !== 16) {
+        setErrorMessage('Credit card number must be 16 digits')
+        return false;
+      }
+      // Remember that the creditCardNumber is a string originally
+      // Reverse the digits of the credit card number
+      const reversedNumbers = sanitizedCard.split('').reverse().map(number => Number(number));
+  
+      const updatedNumbers = [];
+  
+      reversedNumbers.forEach((number, index) => {
+        const isSecondIndex = isOddIndex(index);
+        let updatedNumber = number;
+  
+        if (isSecondIndex) {
+          updatedNumber = updateNumber(number)
+        }
+  
+        updatedNumbers.push(updatedNumber);
+      })
+  
+      const sum = sumNumbers(updatedNumbers);
+  
+      const isDivisibleByTen = checkModuloTen(sum);
+  
+      return isDivisibleByTen;
+    }
+
   // Handlers
   const handleInputChange = (event) => {
     const { name, value } = event.target;
 
     setFormData((prevState) => {
-      return ({
-        ...prevState,
-        [name]: value
-      })
+      const updatedFormData = {...prevState, [name]: value};
+      // Local storage only stores string values so we need to use stringify here to convert it
+      localStorage.setItem('formData', JSON.stringify(updatedFormData))
+      return updatedFormData;
     })
   }
 
@@ -87,11 +115,12 @@ function CheckoutForm() {
     // Default behavior is for the browser to reload the page and send the form data to a server
     event.preventDefault();
 
-    const { name, email, creditCard } = formData;
+    const { name, email } = formData;
 
-    if (!name.length || !email.length || !validateCreditCardNumber(creditCard)) {
+    if (!name.length || !email.length || !validateCreditCardNumber()) {
       setErrorMessage('Invalid input, please try again.')
     } else {
+      setErrorMessage('');
       alert('You have successfully bought an item!')
     }
   }
@@ -110,7 +139,7 @@ function CheckoutForm() {
       <label>
         Credit Card
       </label>
-      <input type='text' name='creditCard' onChange={handleInputChange} value={formData.creditCard}/>
+      <input type='text' name='creditCard' onChange={handleInputChange} value={formData.creditCard} minLength={16} maxLength={16}/>
       {errorMessage && <div style={{ margin: '25px', color: 'red' }}>
         {errorMessage}</div>}
       <button type='submit'>
